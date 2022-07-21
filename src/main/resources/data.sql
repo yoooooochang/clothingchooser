b00711262fa81aab90b9c854629d3bca5ddaa70d
yohkai <m.u.com.yn@gmail.com>
2022-07-21 22:46:06 +0900

    html作成

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO test_ta(name, old)
+VALUES('Taro', 30), ('Jiro', 25), ('Saburo', 22);
---
+++ src/test/java/com/example/clothingchooser/ClothingchooserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clothingchooser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothingchooserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
