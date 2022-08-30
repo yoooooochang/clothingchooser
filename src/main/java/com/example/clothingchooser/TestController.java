0a40b50643821da7ddfc423530f113151b584c99
yohkai <m.u.com.yn@gmail.com>
2022-08-31 00:02:12 +0900

    html画面分作成

--- src/main/java/com/example/clothingchooser/TestController.java
+++ src/main/java/com/example/clothingchooser/TestController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping()
@@ -29,6 +30,12 @@ public class TestController {
         @GetMapping("/top")
         public String top(Model model){
             return "top";
+        }
+
+        @GetMapping("/search")
+        public void searchTemp(Model model, @RequestParam Integer tmp)
+        {
+
         }
     }
 
